30a997bce9d219eab3d1f0c2105f35e0d1b5660e
SanjarbekAI <sanjarbekwork@gmail.com>
2025-06-28 17:54:57 +0500

    added

--- apps/routers/backs.py
+++ apps/routers/backs.py
@@ -1,13 +1,23 @@
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
 
+from apps.filters.is_admin import IsAdmin
+from apps.keyboards.default.admin import admin_main_menu
 from apps.keyboards.default.user import user_main_menu_keyboard
 from apps.states.user import Feedback
+from loader import _
 
 router = Router()
 
 
-@router.message(Feedback.feedback, F.text == "Back ⬅️")
+@router.message(IsAdmin(), F.text.in_(["Back ⬅️"]))
+async def back_admin_main_menu(message: types.Message, state: FSMContext):
+    text = _("Assalomu alaykum, admin 🫡")
+    await message.answer(text=text, reply_markup=admin_main_menu)
+    await state.clear()
+
+
+@router.message(Feedback.feedback, F.text.in_(["Back ⬅️"]))
 async def back_user_main_menu(message: types.Message, state: FSMContext):
     text = "Main menu"
     await message.answer(text=text, reply_markup=await user_main_menu_keyboard())
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import asyncio
 import logging
 
 from aiogram import Bot
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
 
 from apps.middlewares.db_session import DbSessionMiddleware
 from apps.middlewares.language import LanguageMiddleware
 from apps.middlewares.subscription import SubscribeMiddleware
 from apps.routers import start, register, feedback, backs, user_menu
+from apps.routers.admin import category
 from apps.utils.commands import set_my_commands
-from core.config import DEVELOPER, BASE_WEBHOOK_URL
+from core.config import DEVELOPER
 from loader import bot, dp, i18n
 
 # bind localhost only to prevent any external access
@@ -25,11 +25,11 @@ WEBHOOK_SECRET = "SECRET"
 
 async def startup(bot: Bot):
     await set_my_commands(bot)
-    await bot.set_webhook(
-        url=f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
-        secret_token=WEBHOOK_SECRET,
-        drop_pending_updates=True
-    )
+    # await bot.set_webhook(
+    #     url=f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}",
+    #     secret_token=WEBHOOK_SECRET,
+    #     drop_pending_updates=True
+    # )
     await bot.send_message(text="Bot start to work", chat_id=DEVELOPER)
 
 
@@ -37,7 +37,8 @@ async def shutdown(bot: Bot):
     await bot.send_message(text="Bot stopped", chat_id=DEVELOPER)
 
 
-def main():
+async def main():
+    dp.include_router(router=category.router)
     dp.include_router(router=start.router)
     dp.include_router(router=register.router)
     dp.include_router(router=feedback.router)
@@ -51,18 +52,19 @@ def main():
     dp.startup.register(startup)
     dp.shutdown.register(shutdown)
 
-    app = web.Application()
-
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        secret_token=WEBHOOK_SECRET,
-    )
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-
-    setup_application(app, dp, bot=bot)
-
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    # app = web.Application()
+    #
+    # webhook_requests_handler = SimpleRequestHandler(
+    #     dispatcher=dp,
+    #     bot=bot,
+    #     secret_token=WEBHOOK_SECRET,
+    # )
+    # webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    #
+    # setup_application(app, dp, bot=bot)
+    #
+    # web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    await dp.start_polling(bot, polling_timeout=0)
 
 
 if __name__ == '__main__':
@@ -72,4 +74,4 @@ if __name__ == '__main__':
         level=logging.ERROR
     )
     logging.getLogger("aiogram.event").setLevel(logging.ERROR)
-    main()
+    asyncio.run(main())
